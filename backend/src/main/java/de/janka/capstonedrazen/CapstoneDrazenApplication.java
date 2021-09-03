c022291f48383c67933351b7425277e8db90e9ac
janka319 <janka319@gmail.com>
2021-09-03 11:28:29 +0200

    init backend

---
+++ backend/src/main/java/de/janka/capstonedrazen/CapstoneDrazenApplication.java
@@ -0,0 +1,13 @@
+package de.janka.capstonedrazen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneDrazenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneDrazenApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/janka/capstonedrazen/CapstoneDrazenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.janka.capstonedrazen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneDrazenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
