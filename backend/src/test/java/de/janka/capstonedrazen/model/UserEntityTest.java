42f666006c0540f62004927282c95afbd9b6f77d
janka319 <janka319@gmail.com>
2021-09-10 19:04:58 +0200

    add test configuration files and UserEntityTest

---
+++ backend/src/test/java/de/janka/capstonedrazen/model/UserEntityTest.java
@@ -0,0 +1,70 @@
+package de.janka.capstonedrazen.model;
+
+import de.janka.capstonedrazen.SpringBootTests;
+import de.janka.capstonedrazen.repo.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+
+import java.awt.print.PrinterJob;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserEntityTest extends SpringBootTests {
+
+    @Resource
+    private UserRepository userRepository;
+
+    @Test
+    public void testCreateUserWithoutNameShouldThrow() {
+        try {
+            UserEntity userEntity = new UserEntity();
+            userRepository.save(userEntity);
+            fail("user without name must not be persisted");
+        } catch (DataIntegrityViolationException ignore) {
+            // expected
+        }
+    }
+
+    @Test
+    public void testCreateUserName() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName("foo");
+        userEntity.setPassword("password");
+        assertNull(userEntity.getId());
+
+        UserEntity createdEntity = userRepository.save(userEntity);
+        assertNotNull(createdEntity.getId());
+        assertEquals(createdEntity, userEntity);
+
+        // additional equals hash code test by adding same instance to set twice
+        Set<UserEntity> users = new HashSet<>();
+        users.add(userEntity);
+        users.add(createdEntity);
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    public void testUserContainsName() {
+        // GIVEN
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserName("foo");
+        userEntity.setPassword("password");
+        UserEntity expectedUser = userRepository.save(userEntity);
+
+        // WHEN
+        Optional<UserEntity> actualUserOpt = userRepository.findByUserNameContains("o");
+        assertTrue(actualUserOpt.isPresent());
+
+        // THEN
+        assertEquals(expectedUser, actualUserOpt.get());
+    }
+
+}
+
+
