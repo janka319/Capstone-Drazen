b2e1ce544aeee77bd8b29516123aad9c5b497d3e
janka319 <janka319@gmail.com>
2021-09-11 12:53:14 +0200

    delete unused import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Welcome from './pages/Welcome'
 import Settings from './pages/Login'
 import Registration from './pages/Registration'
 import Login from './pages/Login'
