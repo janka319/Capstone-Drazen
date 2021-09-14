f67752e26752dc84c02050b9dd0db9feb815bcb2
janka319 <janka319@gmail.com>
2021-09-14 21:10:43 +0200

    file setup and preparations for security

--- backend/src/main/java/de/janka/capstonedrazen/controller/UserController.java
+++ backend/src/main/java/de/janka/capstonedrazen/controller/UserController.java
@@ -93,7 +93,6 @@ public class UserController {
     private User map(UserEntity userEntity) {
         return User.builder()
                 .userName(userEntity.getUserName())
-                .password(userEntity.getPassword())
                 .build();
     }
 
