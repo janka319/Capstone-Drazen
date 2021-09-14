e2f0cf976ae0b9ba53a7b3d48463e2931b1a4aa1
janka319 <janka319@gmail.com>
2021-09-14 21:05:44 +0200

    fix on AccessToken, AuthController, Credentials, JwtConfig, JwtService, SecurityConfig, SwaggerConfig and UserEntityDetailsService

---
+++ backend/src/main/java/de/janka/capstonedrazen/service/JwtService.java
@@ -0,0 +1,50 @@
+package de.janka.capstonedrazen.service;
+
+
+import de.janka.capstonedrazen.config.JwtConfig;
+import de.janka.capstonedrazen.model.UserEntity;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    private final JwtConfig jwtConfig;
+
+    @Autowired
+    public JwtService(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    public String createJwtToken(UserEntity user) {
+        Instant now = Instant.now();
+        Date iat = Date.from(now);
+        Date exp = Date.from(now.plus(Duration.ofMinutes(jwtConfig.getExpiresAfterMinutes())));
+
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("role", user.getRole());
+        return Jwts.builder()
+                .setClaims(new HashMap<>())
+                .setSubject(user.getUserName())
+                .setIssuedAt(iat)
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS256, jwtConfig.getSecret())
+                .compact();
+    }
+
+    public Claims getClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(jwtConfig.getSecret())
+                .parseClaimsJws(token).getBody();
+    }
+}
